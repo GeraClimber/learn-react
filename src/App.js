526dafd8fb4fa34137f1da57b2a4995dedc86fe9
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-05-07 09:53:53 +0300

    add change props.item.id

--- src/App.js
+++ src/App.js
@@ -8,10 +8,20 @@ class App extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        // Обновите состояние так, чтобы у элемента с заданным id свойство 
+        // completed поменялось бы c false на true (или наоборот).
+        // Помните о том, что предыдущую версию состоянию менять не следует.
+        // Вместо этого нужно вернуть новую версию состояния, содержащую изменения.
+        // (Подумайте о том, как для этого использовать метод массивов map.)
+        console.log("Changed ", id);
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
         
         return (
             <div className="todo-list">
