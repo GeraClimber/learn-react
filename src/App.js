87c02a4b7e3ff146f842568149e06ca415332eb7
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-05-07 11:02:17 +0300

    posibility to check

--- src/App.js
+++ src/App.js
@@ -12,13 +12,19 @@ class App extends React.Component {
     }
 
     handleChange(id) {
-        // Обновите состояние так, чтобы у элемента с заданным id свойство 
-        // completed поменялось бы c false на true (или наоборот).
-        // Помните о том, что предыдущую версию состоянию менять не следует.
-        // Вместо этого нужно вернуть новую версию состояния, содержащую изменения.
-        // (Подумайте о том, как для этого использовать метод массивов map.)
-        console.log("Changed ", id);
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
+    
     
     render() {
         const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
